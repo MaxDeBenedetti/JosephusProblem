9b127abc3f2bc1143636c57d69e7c7cc8863b8a0
MaxDeBenedetti <modsme@gmail.com>
2018-03-12 20:19:44 -0500

    Initial commit

---
+++ Assets/Editor/NewEditModeTest.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewEditModeTest {
+
+	[Test]
+	public void NewEditModeTestSimplePasses() {
+        // Use the Assert class to test conditions.
+        Assert.Pass();
+	}
+
+    [Test]
+    public void JosephusTestWorks()
+    {
+        Josephus joe = GameObject.FindObjectOfType<Josephus>();
+        joe.output.text = "something";
+        Assert.That(joe.output.text.Equals("something"), "Test works");
+    }
+
+    [Test]
+    public void JosephusOnlyAcceptsNumericInput()
+    {
+        Josephus joe = GameObject.FindObjectOfType<Josephus>();
+        joe.output.text = "hi";
+        joe.JosephusAlgorithm("a");
+        Assert.That(joe.output.text.Equals(""));
+    }
+
+    [Test]
+    public void JosephusOnlyAcceptsPositiveInput()
+    {
+        Josephus joe = GameObject.FindObjectOfType<Josephus>();
+        joe.JosephusAlgorithm("-1");
+        Assert.That(joe.output.text.Equals(""));
+    }
+
+    [Test]
+    public void JosephusI1O1()
+    {
+        Josephus joe = GameObject.FindObjectOfType<Josephus>();
+        joe.JosephusAlgorithm("1");
+        Assert.That(joe.output.text.Equals("1"));
+    }
+
+    [Test]
+    public void JosephusI2O1()
+    {
+        Josephus joe = GameObject.FindObjectOfType<Josephus>();
+        joe.JosephusAlgorithm("2");
+        Assert.AreEqual(joe.output.text, "1");
+    }
+
+    [Test]
+    public void JosephusI5O3()
+    {
+        Josephus joe = GameObject.FindObjectOfType<Josephus>();
+        joe.JosephusAlgorithm("5");
+        Assert.AreEqual(joe.output.text, "3", "value was: "+joe.output.text);
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+	public IEnumerator NewEditModeTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
---
+++ Assets/Josephus.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Josephus : MonoBehaviour {
+
+    public InputField input;
+    public Text output;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AcceptInput()
+    {
+        JosephusAlgorithm(input.text);
+    }
+
+    //parses input string then sets output to the Josephus answer for the given number
+    //will output an empty string for non-numeric input
+    public void JosephusAlgorithm(string input)
+    {
+        int noOfknights;
+        bool valid = int.TryParse(input, out noOfknights);
+        valid = valid && noOfknights > 0;
+        if (valid){
+            output.text = fastJosephus(noOfknights);
+        }
+        else
+        {
+            output.text = "";
+        }
+    }
+
+    public string slowJosephus(int numOfKnights)
+    {
+        //actual algorithm goes here
+        Knight[] knights = new Knight[numOfKnights];
+        knights[0] = new Knight();
+        knights[0].Position = 1;
+        for (int i = 1; i < knights.Length; i++)
+        {
+            knights[i] = new Knight();
+            knights[i].Position = i + 1;
+            knights[i - 1].NextKnight = knights[i];
+        }
+        knights[knights.Length - 1].NextKnight = knights[0];
+        Knight myKnight = knights[0];
+        while (myKnight.NextKnight != myKnight)
+        {
+            myKnight.NextKnight = myKnight.NextKnight.NextKnight;
+            myKnight = myKnight.NextKnight;
+        }
+        return myKnight.Position + "";
+    }
+
+    public string fastJosephus(int numOfKnights)
+    {
+        string binaryForm = System.Convert.ToString(numOfKnights, 2);
+        string answer = binaryForm.Substring(1, binaryForm.Length - 1) + binaryForm.Substring(0, 1);
+        return System.Convert.ToInt32(answer, 2) + "";
+    }
+
+    private class Knight
+    {
+        private int position;
+        private Knight nextKnight;
+
+        public Knight NextKnight
+        {
+            get
+            {
+                return nextKnight;
+            }
+
+            set
+            {
+                nextKnight = value;
+            }
+        }
+
+        public int Position
+        {
+            get
+            {
+                return position;
+            }
+
+            set
+            {
+                position = value;
+            }
+        }
+
+        public Knight(int position, Knight nextKnight)
+        {
+            this.Position = position;
+            this.NextKnight = nextKnight;
+        }
+
+        public Knight()
+        {
+        }
+    }
+
+}
+
+
